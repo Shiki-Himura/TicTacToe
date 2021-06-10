f3ec27b497815e408f928f16951d416ced546c49
Shiki-Himura <athaus@hotmail.de>
2021-06-10 12:54:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+var playField = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+
+var playerOne = true;
+var hasWon = false;
+
+function buttonClick(e, x, y){
+    console.log(e);
+    console.log(x,y);    
+
+    if(playerOne)
+    {
+        e.target.textContent = "X";
+        playField[x][y] = 1;
+        
+        playerOne = false;
+    }
+    else
+    {
+        e.target.textContent = "O";
+        playField[x][y] = 2;
+
+        playerOne = true;
+    }
+    e.target.disabled = true;
+    validateWinner();
+    
+}
+
+function reset(){
+    window.location.reload();
+}
+
+function winnerPrompt(){
+    //TODO - declare winner popup
+    window.prompt("You´ve won!")
+}
+
+function validateWinner(){
+    for(var i = 0; i < playField.length; i++)
+    {
+        var b = document.getElementsByClassName("gamebtn");
+
+        //determine if column or row has won
+        if(playField[i][0] != 0 && playField[i][0] == playField[i][1] && playField[i][0] == playField[i][2])
+        {
+            for(var i = 0; i < b.length; i++)
+            {
+                b[i].disabled = true;
+            }
+        }
+
+        //determine if column or row has won
+        if(playField[0][i] != 0 && playField[0][i] == playField[1][i] && playField[0][i] == playField[2][i])
+        {
+            for(var i = 0; i < b.length; i++)
+            {
+                b[i].disabled = true;
+            }
+        }
+
+        //determine if the diagonal[0][0] to [2][2] has won
+        if(playField[0][0] != 0 && playField[1][1] == playField[0][0] && playField[2][2] == playField[0][0])
+        {
+            for(var i = 0; i < b.length; i++)
+            {
+                b[i].disabled = true;
+            }
+        }
+
+        //determine the diagonal[0][2] to [2][0] has won
+        if(playField[0][2] != 0 && playField[1][1] == playField[0][2] && playField[2][0] == playField[0][2])
+        {
+            for(var i = 0; i < b.length; i++)
+            {
+                b[i].disabled = true;
+            }
+        }
+    }
+}
+
